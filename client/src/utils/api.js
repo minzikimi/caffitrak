086076ca80642565a2bb5ce5de6e2a80a5b970c2
minzikimi <minji.kim@hyperisland.se>
2025-04-17 12:36:43 +0200

    fix : minor change

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://caffitrak-backend.onrender.com/api/drinks", 
+    baseURL: "http://localhost:5004/api/drinks",
     headers: {
         "content-type": "application/json",
     },
 });
-
 export default api;
